0ba561eb246aeaebb1b13068e410ada998ba587c
Evgeny <e.ivakhnenko@gmail.com>
2026-01-20 12:16:15 +0700

    Update config

--- bot_config.py
+++ bot_config.py
@@ -7,7 +7,7 @@ load_dotenv()
 # Получаем токен из переменных окружения
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 ADMIN_ID = int(os.getenv('ADMIN_ID', 0))  # 0 если не установлено
-DATABASE_URL = os.getenv('DATABASE_URL', 'C:\\Documents\\Labirint_bot\\labirint.db')
+DATABASE_URL = os.getenv('DATABASE_URL', 'labirint.db')
 
 # Проверка что токен установлен
 if not TELEGRAM_BOT_TOKEN:
